0caf90dc37a4ec46c8bb4cefb192086374693639
cosmomathieu <cosmo@cosmointeractive.co>
2021-01-08 16:28:03 -0500

    Add command to run server as node package

--- postinstall.js
+++ postinstall.js
@@ -3,13 +3,18 @@ const saveFile = require('fs').writeFileSync;
 const pkgJsonPath = require.main.paths[0].split('node_modules')[0] + 'package.json';
 const json = require(pkgJsonPath);
 
+if (!json.hasOwnProperty('dependencies')) {
+  json.dependencies = {};
+}
+
 if (!json.hasOwnProperty('scripts')) {
   json.scripts = {};
 }
 
+json.dependencies['npm-run-all'] = "^4.1.5";
 json.scripts['sass:build'] = "node-sass --output-style compressed src/scss/style.scss -o build/assets/css --source-map true";
 json.scripts['sass:watch'] = "node-sass src/scss/style.scss -wo build/assets/css --source-map true";
-json.scripts['start'] = "node ./node_modules/@pagestudiocms/pagestudio-node-development-server/server.js";
+json.scripts['start'] = "npm-run-all --parallel sass:watch start-server",
 json.scripts['start-server'] = "node ./node_modules/@pagestudiocms/pagestudio-node-development-server/server.js";
 
 saveFile(pkgJsonPath, JSON.stringify(json, null, 2));
