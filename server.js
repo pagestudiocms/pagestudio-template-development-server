3815196a10dd4d3cd7be28e234f4f392799869c4
cosmomathieu <cosmo@cosmointeractive.co>
2025-02-02 23:42:39 -0500

    Refactor the server and clean up code

--- server.js
+++ server.js
@@ -8,42 +8,57 @@
 
 "use strict";
 
-// Setup environment
-
 const chalk = require('chalk');
 const figlet = require('figlet');
 const liveServer = require("live-server");
 const fs = require('fs'); // Added fs to check if 'dist' exists
 const path = require("path"); 
+const args = require('command-line-args');
 
-// Setup paths 
+// Setup defaults and options
+
+const meta = {
+  VERSION: '2.0.0-beta1',
+  PUBLISHER: 'PageStudioCMS',
+  YEAR: '2025',
+}
 
 const currentDir = __dirname;
-const templateProjectRoot = path.resolve(currentDir, '../pagestudiocms-templates/frontsite-template');
-
 const abspath = path.join(__dirname, './');
 const binpath = path.join(abspath, 'bin/');
 const libpath = path.join(abspath, 'lib/');
-// const webroot = path.join(templateProjectRoot, 'dist/'); // dist folder in your test project
-const webroot = path.join(abspath, 'dist/');
 
-console.log('Project Directory:', currentDir);
-console.log('Template Directory:', webroot);
+const optionDefinitions = [
+  { name: 'watch', alias: 'w', type: String, defaultValue: 'dist,src/html' },
+  { name: 'host', alias: 'h', type: String, defaultValue: '127.0.0.1' },
+  { name: 'root', alias: 'r', type: String, defaultValue: 'dist' },
+  { name: 'port', alias: 'p', type: String, defaultValue: '8080' },
+];
+const options = args(optionDefinitions);
 
-// Setup defaults 
+const webroot = path.resolve(process.cwd(), options.root);
+const watchDirectories = options.watch.split(',').map(dir => dir.trim()); // Convert the watch string into an array if it's provided
+
+// var homeDir = process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'];
+
+// Setup environment
 
 const ignore = 'app,bin,build,etc,lib,node_modules,src,var';
 
-const defaults = {
-  port: process.env.PORT || 8080,         // Set the server port. Defaults to 8080.
-  host: process.env.HOST || "127.0.0.1",  // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
-  root: webroot,           // Set root directory that's being served. Defaults to cwd.
-  open: true,              // When false, it won't load your browser by default.
+/**
+ * Live Server Parameters
+ */
+const liveServerParams = {
+  port: options.port,
+  host: options.host,
+  root: webroot,            // Set root directory that's being served. Defaults to cwd.
+  open: true,               // When false, it won't load your browser by default.
   ignore: ignore,           // comma-separated string for paths to ignore
   // file: 'default.html',    // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
   wait: 2000,               // Waits for all changes, before reloading. Defaults to 0 sec.
   mount: [
     ['/assets', './dist/assets'],
+    ['/images', './demo/images'],
   ],                        // Mount a directory to a route.
   logLevel: 2,              // 0 = errors only, 1 = some, 2 = lots
   headers: {
@@ -51,25 +66,46 @@ const defaults = {
     "Pragma": "no-cache",
     "Expires": "0",
   },
-  middleware: [], // An array of Connect-compatible middleware
-  watch: [
-    'dist', 
-    'src/html'
-  ]
+  middleware: [],           // An array of Connect-compatible middleware
+  watch: watchDirectories,  // Paths to exclusively watch for changes. Defaults to dist,src/html
+};
+
+const preflightChecks = () => {
+  // Check if the dist folder exists
+  if (!fs.existsSync(webroot)) {
+    throw new Error(`Invalid value for option '--webroot'. The specified path "${webroot}" does not exist.`);
+  }
+}
+
+// Function to print the ASCII art with styled text
+const printStartupMessage = () => {
+  const asciiArt = `
+   ____                           ____    _                 _   _ 
+  |  _ \\    __ _    __ _    ___  / ___|  | |_   _   _    __| | (_)   ___  
+  | |_) |  / _\` |  / _\` |  / _ \\ \\___ \\  | __| | | | |  / _\` | | |  / _ \\ 
+  |  __/  | (_| | | (_| | |  __/  ___) | | |_  | |_| | | (_| | | | | (_) |
+  |_|      \\__,_|  \\__, |  \\___| |____/   \\__|  \\__,_|  \\__,_| |_|  \\___/ 
+                    |___/                                 
+                                            © ${meta.YEAR} ${meta.PUBLISHER}
+                                            ${chalk.cyan(`Local Development Environment`)}
+                                            ${chalk.magenta('Version')} ${chalk.magenta(meta.VERSION)}\n\n`;
+
+  console.log(chalk.green(`Starting Development Server\n`));
+  console.log(chalk.yellow(asciiArt));
+  // console.log(chalk.green(`Server root directory ${currentDir}`));
+  // console.log(chalk.green(`Process current working directory ${process.cwd()}`));
 };
 
 // Start live server
 try {
-  liveServer.start(defaults); // Starting the live server
-  console.log(
-    chalk.yellow(
-      figlet.textSync('PageStudio', { horizontalLayout: 'full' })
-    )
-  );
-  console.log(chalk.green(`Server listening on http://${defaults.host}:${defaults.port}`));
+  printStartupMessage();
+
+  preflightChecks();
+
+  liveServer.start(liveServerParams); // Starting the live server
 } catch (error) {
-  console.error(chalk.red('Error starting live-server:'), error);
-  // process.exit(1); // Exit with error
+  console.error(chalk.red("Error starting pagestudio-server:"), error);
+  process.exit(1); // Exit with error
 }
 
 // Graceful shutdown
