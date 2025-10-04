f5976c439fcf5d62f0bdcc3e4e42217f90e07d00
Cosmo Mathieu <cosmo.mathieu_ext@michelin.com>
2025-10-04 00:06:23 -0400

    Add example template for development

---
+++ example/js/main.js
@@ -0,0 +1,3 @@
+const VERSION = window.VERSION || '2.4.0';
+console.log('%cProudly Crafted with PageStudioCMS.', 'background: #222; color: #bada55');
+console.log(`Template version ${this.VERSION}`); 
